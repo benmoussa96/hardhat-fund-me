ac7b2f3f4646639ca87134b3a9243e30b54dae14
benmoussa96 <ghaiethbmoussa@gmail.com>
2023-01-06 16:05:26 +0100

    Contract cleanup according to best paractices

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,34 +3,49 @@ pragma solidity 0.8.17;
 
 import "./PriceConverter.sol";
 
-error NotOwner();
-error FundAmountNotEnough();
-error CallFailed();
+error FundMe__NotOwner();
+error FundMe__FundAmountNotEnough();
+error FundMe__CallFailed();
 
+/**
+ * @title A contract for crowd funding
+ * @author Ghaieth BEN MOUSSA
+ * @notice To demo a sample funding contract
+ * @dev Implements price feeds as a library
+ */
 contract FundMe {
     using PriceConverter for uint256;
 
     AggregatorV3Interface public priceFeed;
 
     address public immutable i_owner;
-    // 21,508 gas - immutable
-    // 23,644 gas - non-immutable
 
     uint256 public constant MINIMUM_USD = 50 * 1e18;
-    // 21,415 gas - constant
-    // 23,515 gas - non-constant
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) { revert FundMe__NotOwner(); }
+        _;
+    }
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
     function fund() public payable {
         // We want to be able to set a minimum fund amount of 50 USD
-        if(msg.value.getConversionRate(priceFeed) < MINIMUM_USD) { revert FundAmountNotEnough();}
+        if(msg.value.getConversionRate(priceFeed) < MINIMUM_USD) { revert FundMe__FundAmountNotEnough();}
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
@@ -46,38 +61,10 @@ contract FundMe {
         // Reset the array
         funders = new address[](0);
 
-        // You can send Ether to other contracts by converting msg.sender to a payable address, then:
-        // - transfer (2300 gas, throws error)
-        // - send (2300 gas, returns bool)
-        // - call (forward all gas or set gas, returns bool)
-
-        // Transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // Send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed!");
-
         // Call
         (
             bool callSuccess, /*bytes memory dataReturned*/
-
         ) = payable(msg.sender).call{value: address(this).balance}("");
-        if(!callSuccess) { revert CallFailed(); }
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) { revert NotOwner(); }
-        _;
-    }
-
-    // What happens if someone sends ETH to this
-    // contract without calling the fund() function:
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
+        if(!callSuccess) { revert FundMe__CallFailed(); }
     }
 }
