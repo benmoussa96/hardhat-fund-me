8256295ec23560622eab8951efb3dfef844a3d8d
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-11 20:00:05 +0100

    Adding Ethers.js

--- client/index.js
+++ client/index.js
@@ -1,11 +1,27 @@
+import { ethers } from "./ethers-5.2.esm.min.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+
 const connect = async () => {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    connectButton.innerHTML = "Connected!";
     console.log("Connected!");
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "No Metamask detected!";
+    connectButton.innerHTML = "No Metamask detected!";
     console.log("No Metamask detected!");
   }
 };
+
+const fund = async (ethAmount) => {
+  console.log(`Funding with ${ethAmount}`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log(signer);
+  }
+};
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
